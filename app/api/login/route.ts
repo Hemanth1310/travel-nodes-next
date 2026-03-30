d2073ab4c9e5e5fb11ddd9394dd23d1faf0398db
Hemanth1310 <hemanth13k@gmail.com>
2026-03-30 11:56:43 +0200

    backend-login-route

---
+++ app/api/login/route.ts
@@ -0,0 +1,47 @@
+import { Prisma } from "@/generated/prisma/client";
+import { prisma } from "@/lib/prisma";
+import { loginDetailSchema } from "@/lib/typechecker";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req:NextRequest){
+    const body = await req.json()
+    const bodyParsed = loginDetailSchema.safeParse(body)
+
+    if(!bodyParsed.success){
+        return NextResponse.json({error:bodyParsed.error.issues[0].message},{status:400})
+    }
+
+    try{
+        const user = await prisma.user.findUnique({
+            where:{
+                email:bodyParsed.data.email
+            }
+        })
+
+        if(!user){
+            return NextResponse.json({error:"User not found"},{status:404})
+        }
+
+        const isPasswordMatched = bcrypt.compare(bodyParsed.data.password,user.password)
+
+        if(!isPasswordMatched){
+            return NextResponse.json({error:"Invalid Password"},{status:403})
+        }
+
+        return NextResponse.json({
+            message:"Login Successful",
+            payload:{...user}
+        },{status:400})
+        
+    }catch(error){
+        if(error instanceof Prisma.PrismaClientKnownRequestError){
+             if(error.code === '2025'){
+                return NextResponse.json({error:"User not found"},{status:404})
+            }
+        }
+
+        return NextResponse.json({error:"Unexpected Error Occurred"},{status:500})
+    }
+}
+
