3fb67bcf675e9ca1b28bfe5532b0e9f4ed6ed1ed
Hemanth1310 <hemanth13k@gmail.com>
2026-03-30 13:36:23 +0200

    backend-login-jwt-session-handling

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -3,6 +3,10 @@ import { prisma } from "@/lib/prisma";
 import { loginDetailSchema } from "@/lib/typechecker";
 import bcrypt from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
+import jwt, { JsonWebTokenError } from "jsonwebtoken";
+import { cookies } from "next/headers";
+
+const JWT_secret = process.env.JWT_secret || "123456789";
 
 export async function POST(req:NextRequest){
     const body = await req.json()
@@ -29,15 +33,41 @@ export async function POST(req:NextRequest){
             return NextResponse.json({error:"Invalid Password"},{status:403})
         }
 
+        const {firstName,lastName,email,id,imagePath,isVerified} = user
+
+        const tokenPayload = {
+            email:user.email,
+            userId: user.id
+        }
+
+        const token  = jwt.sign(tokenPayload,JWT_secret,{expiresIn:"1hr"})
+
+        const cookieStore = await cookies()
+        
+        cookieStore.set('session-token',token,{
+            httpOnly:true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite:'lax',
+            path:'/',
+            maxAge: 60*60*24
+        })
         return NextResponse.json({
             message:"Login Successful",
-            payload:{...user}
+            payload:{firstName,lastName,email,id,imagePath,isVerified}
         },{status:200})
         
     }catch(error){
         if(error instanceof Prisma.PrismaClientKnownRequestError){
              if(error.code === '2025'){
                 return NextResponse.json({error:"User not found"},{status:404})
+            }
+        }
+
+        if(error instanceof JsonWebTokenError){
+            if (error.name === "TokenExpiredError" || error.name === "JsonWebTokenError") {
+            return NextResponse.json({
+                error: "User token has expired or is not valid",
+                },{status:403});
             }
         }
 
