327bd54c05fd850bb01a9484e0345f872bd5c676
Hemanth1310 <hemanth13k@gmail.com>
2026-03-30 12:01:54 +0200

    backend-login-route-bugfix

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -32,7 +32,7 @@ export async function POST(req:NextRequest){
         return NextResponse.json({
             message:"Login Successful",
             payload:{...user}
-        },{status:400})
+        },{status:200})
         
     }catch(error){
         if(error instanceof Prisma.PrismaClientKnownRequestError){
