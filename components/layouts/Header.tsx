0c995be3646124bf543c8e86170cd5681ae1d927
Hemanth1310 <hemanth13k@gmail.com>
2026-03-31 14:21:42 +0200

    frontend-header

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import {Luggage} from 'lucide-react'
 import Link from 'next/link'
 import { User } from '@/lib/types'
+import SearchBar from './SearchBar'
+import UserDropMenu from './UserDropMenu';
+
 type HeaderProps = {
   user: User | null;
 };
@@ -9,19 +11,14 @@ type HeaderProps = {
 const Header = ({ user }: HeaderProps) => {
   
   return (
-    <div className='w-full h-16 shadow-md flex items-center justify-center'>
-        <div className='container w-full h-full flex items-center justify-between'>
-          <div className='flex items-center gap-2'>
-            <div className='h-8 w-8 flex items-center justify-center bg-linear-to-r from-blue-500 to-purple-500 rounded-lg'>
-              <Luggage className='h-6 w-6 text-white' />
-            </div>
-            <span className='text-xl font-bold text-blue-500 font-mono'>Traveler Node</span>
+    <div className='fixed left-22 right-0 top-0 h-16 p-3 flex items-center gap-4 bg-mist-50'>
+        <div className='container w-full h-full flex items-center justify-between gap-4'>
+          <div className='w-full h-full'>
+            <SearchBar/>
           </div>
           <div >
-            {!user?<Link className='text-lg ' href='/login'>Login
-            </Link>:<div className='h-8 w-8 cursor-pointer text-center rounded-full bg-blue-500 flex items-center justify-center'>
-              <span className='text-lg text-white'>H</span>
-              </div>}
+            {false?<Link className='text-lg ' href='/login'>Login
+            </Link>:<UserDropMenu user={user}/>}
           </div>
         </div>
     </div>
