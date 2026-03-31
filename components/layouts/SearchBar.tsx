baa04bb78c450bce78d4a266023c5b4b260fd8e4
Hemanth1310 <hemanth13k@gmail.com>
2026-03-31 14:31:52 +0200

    frontend-searchbar-debouncing

--- components/layouts/SearchBar.tsx
+++ components/layouts/SearchBar.tsx
@@ -1,10 +1,12 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 
 const SearchBar = () => {
     const [inputVal, setInputVal] = useState("")
     const [isOpen, setIsOpen] = useState(false) 
+    const divRef = useRef<HTMLDivElement | null>(null)
 
+   
     useEffect(()=>{
         const timeoutId = setTimeout(()=>{
             if(inputVal.length>=3){
@@ -15,8 +17,22 @@ const SearchBar = () => {
 
         return ()=>clearTimeout(timeoutId)
     },[inputVal])
+
+    useEffect(()=>{
+
+          const handleMouseDown =(e: MouseEvent)=>{
+                if (divRef.current && !divRef.current.contains(e.target as Node)) {
+                    setIsOpen(false)
+                }
+            }
+        window.addEventListener("mousedown",handleMouseDown)
+    return () => window.removeEventListener('mousedown', handleMouseDown)
+
+    },[])
+
+   
   return (
-    <div className='h-full w-full rounded-2xl flex bg-mist-200 relative bg-amber-400'>
+    <div ref={divRef} className='h-full w-full rounded-2xl flex bg-mist-200 relative'>
         <input suppressHydrationWarning className='ml-5 w-full text-lg outline-none focus:outline-none bg-transparent' type='text' value={inputVal} onChange={(e)=>setInputVal(e.target.value)}></input>
         {isOpen &&   <div className='absolute min-h-10 top-11 w-full bg-mist-100 rounded-2xl p-5 text-xl'>
             kfel
