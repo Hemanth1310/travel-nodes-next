da9c91c468f51ca59623efddf554ba9d73f0303d
Hemanth1310 <hemanth13k@gmail.com>
2026-04-02 14:08:23 +0200

    typechecker-node-update

--- lib/typechecker.ts
+++ lib/typechecker.ts
@@ -1,6 +1,5 @@
-// import { tag } from '@prisma/client';
 import { z } from 'zod';
-
+import { tag as PrismaTag } from '@/generated/prisma/enums';
 // Defining Email rules 
 const emailRules = z.string().email("Invalid email format").trim().toLowerCase()
 
@@ -20,3 +19,21 @@ export const registerSchema = z.object({
     lastName: z.string().min(1,"Last Name cannot be empty"),
     password: passwordRules,
 })
+
+
+export const tagSchema = z.enum(PrismaTag)
+export type Tag = z.infer<typeof tagSchema>
+export const NodeSchema = z.object({
+  title: z.string().min(3, "Title is too short").max(100),
+  coordinates: z.string().min(3,"Please enter valid coordinates"),
+  content: z.string().min(10, "Content must be more descriptive"),
+  
+  // Coerce converts the form string "2024-10-01" into a JS Date object
+  visitDate: z.coerce.date(),
+  
+  // Handling arrays from FormData can be tricky; see step 2
+  tags:z.array(tagSchema).min(1,"Select atleast one tag"),
+  
+  // Image is handled as a File object in the browser
+  image: z.instanceof(File).refine((file) => file.size < 5000000, "Max size is 5MB")
+});
